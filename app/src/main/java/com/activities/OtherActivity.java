485883326f2a8764ad475754ceac8c290814f239
agvrn <63383886+agvrn@users.noreply.github.com>
2020-04-13 17:33:10 +0700

    Revert "Merge branch 'rian'"
    This reverts commit d694bcd8d7c82a871fb40a2be941ca49e9b39875, reversing
    changes made to 1f2dcfb0b8b7600ffbcddf0ce2a3426cc5989d26.

---
+++ app/src/main/java/com/activities/OtherActivity.java
@@ -0,0 +1,68 @@
+package com.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class OtherActivity extends AppCompatActivity {
+
+    private Button roomButton;
+    private Button courtButton;
+    private Button musicButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+
+        getSupportActionBar().setTitle("Other Reservation");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        roomButton = (Button)findViewById(R.id.button_room);
+        roomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRoomActivity();
+            }
+        });
+
+        courtButton = (Button)findViewById(R.id.button_court);
+        courtButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCourtActivity();
+            }
+        });
+
+        musicButton = (Button)findViewById(R.id.button_music);
+        musicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMusicActivity();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        //disables back button
+    }
+
+    public void openRoomActivity(){
+        Intent intent = new Intent(this, RoomActivity.class);
+        startActivity(intent);
+    }
+
+    public void openCourtActivity(){
+        Intent intent = new Intent(this, CourtActivity.class);
+        startActivity(intent);
+    }
+
+    public void openMusicActivity(){
+        Intent intent = new Intent(this, MusicActivity.class);
+        startActivity(intent);
+    }
+}
