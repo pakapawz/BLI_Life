6c224ba6d73c46b1a9bc11cc5200ea370cceb703
agvrn <63383886+agvrn@users.noreply.github.com>
2020-04-20 15:58:51 +0700

    update account
    bisa gnti nama

--- app/src/main/java/com/activities/AccountActivity.java
+++ app/src/main/java/com/activities/AccountActivity.java
@@ -3,11 +3,14 @@ package com.activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,22 +18,29 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.annotation.Nullable;
 
 public class AccountActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView userName, userEmail, userClass, userAccount, verfyMsg, ver;
-    private Button buttonLogout, buttonVerifyNow;
+    private EditText editUname;
+    private Button buttonLogout, buttonVerifyNow, buttonsaveChanges;
 
     private FirebaseAuth firebaseAuth;
     private FirebaseFirestore fireStore;
-    private String userId;
+
+    private String userId, editUsername;
+    private ProgressDialog progressUpdating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,14 +52,20 @@ public class AccountActivity extends AppCompatActivity implements View.OnClickLi
         userName         = findViewById(R.id.textView_ProfileUsername);
         userEmail        = findViewById(R.id.textView_ProfileEmail);
         userClass        = findViewById(R.id.textView_ProfileClass);
+        editUname        = findViewById(R.id.editText_editUsername);
 
         firebaseAuth     = FirebaseAuth.getInstance();
         fireStore        = FirebaseFirestore.getInstance();
 
+        buttonsaveChanges= findViewById(R.id.button_saveChanges);
         buttonVerifyNow  = findViewById(R.id.button_verifyNow);
         verfyMsg         = findViewById(R.id.textView_VerifyMsg);
         ver              = findViewById(R.id.textView_Verify);
+        progressUpdating = new ProgressDialog(this);
 
+        buttonsaveChanges.setOnClickListener(this);
+        buttonLogout     = findViewById(R.id.button_logout);
+        buttonLogout.setOnClickListener(this);
 
         userId           = firebaseAuth.getCurrentUser().getUid();
 
@@ -67,7 +83,7 @@ public class AccountActivity extends AppCompatActivity implements View.OnClickLi
                     userVer.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
                         @Override
                         public void onSuccess(Void aVoid) {
-                            Toast.makeText(v.getContext(), "Verification Email has been sent.", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(v.getContext(), "Verification Email has been sent. ", Toast.LENGTH_SHORT).show();
                             logout();
                         }
                     }).addOnFailureListener(new OnFailureListener() {
@@ -94,12 +110,38 @@ public class AccountActivity extends AppCompatActivity implements View.OnClickLi
                 userClass.setText("Class    : " + documentSnapshot.getString("uKelas"));
             }
         });
-
-        buttonLogout  = findViewById(R.id.button_logout);
-        buttonLogout.setOnClickListener(this);
-
         getSupportActionBar().setTitle("Account Setting");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private boolean validateUsername(String username){
+        //username empty
+        if(TextUtils.isEmpty(username)){
+            //password empty
+            editUname.setError("Username is required.");
+            return false;
+        }
+        //username too short
+        if(username.length() < 4 || username.length() > 15){
+            editUname.setError("Username must be at least 4 - 15 Characters.");
+            return false;
+        }
+
+        //contains whitespace
+        if(username.contains(" ")){
+            editUname.setError("Username cannot contain whitespaces.");
+            return false;
+        }
+
+        return true;
+    }
+
+    public void updateUsername() {
+        editUsername = editUname.getText().toString().trim();
+        if(validateUsername(editUsername) == false) return;
+        userId = firebaseAuth.getCurrentUser().getUid();
+        DocumentReference documentReference = fireStore.collection("users").document(userId);
+        documentReference.update("uName", editUsername);
     }
 
     public void logout(){
@@ -112,6 +154,12 @@ public class AccountActivity extends AppCompatActivity implements View.OnClickLi
     public void onClick(View view){
         if(view == buttonLogout){
             logout();
+        }
+        if(view == buttonsaveChanges){
+            updateUsername();
+            progressUpdating.setMessage("Please wait ...");
+            progressUpdating.show();
+            startActivity(new Intent(getApplicationContext(),AccountActivity.class));
         }
     }
 
